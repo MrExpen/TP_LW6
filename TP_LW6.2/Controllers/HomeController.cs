e1a4429a40fb9213058ae2cd99400a192b1c14ac
Dmitrii Chibrikov <chi-dimochka@yandex.ru>
2023-06-02 13:29:40 +0400

    Initial Commit

---
+++ TP_LW6.2/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace TP_LW6._2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Index")]
+        public ActionResult PostIndex(long depositAmount, DateTime birthday)
+        {
+            int? id = null;
+            if (int.TryParse(HttpContext.Request.Form["id"] ?? string.Empty, out var value))
+                id = value;
+            
+
+            if (id is null)
+                return RedirectToAction(nameof(Index));
+
+            ViewBag.Id = id;
+            ViewBag.FirstName = HttpContext.Request.Form["firstName"];
+            ViewBag.LastName = HttpContext.Request.Form["lastName"];
+            ViewBag.Patronymic = HttpContext.Request.Form["patronymic"];
+            ViewBag.DepositAmount = depositAmount;
+            ViewBag.Birthday = birthday;
+
+            return View("Display");
+        }
+
+    }
+}
---
+++ TP_LW6.2/Controllers/MyController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace TP_LW6._2.Controllers
+{
+    public class MyController : IController
+    {
+        public void Execute(RequestContext requestContext)
+        {
+            if (requestContext.RouteData.Values["action"].Equals("Start") &&
+                requestContext.RouteData.Values["id"].Equals("0"))
+            {
+                requestContext.HttpContext.Response.Redirect("/Home/Index", true);
+                return;
+            }
+
+            requestContext.HttpContext.Response.Write(
+                $"<h2>Неверные параметры Action и Id<br/>{requestContext.HttpContext.Request.Url}</h2>");
+        }
+    }
+}
